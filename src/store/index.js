a780cefd05a799e53983cbfe4c126b6037a4ca41
Carlos Viteri <carlos.viteri@verndale.com>
2020-04-04 00:33:15 -0500

    Create vue keep

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,15 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    notes: []
+  },
+  mutations: {
+    addNewNote(state, payload) {
+      state.notes = [...state.notes, payload]
+    },
+    deleteNote(state, id) {
+      state.notes = state.notes.filter(note => note.id !== id);
+    }
+  }
 });
